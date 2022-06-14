0c81f35ad874d7b1712682f1671fd971df452a56
Kamogelo Khumalo <kamokhumalo04@gmail.com>
2022-06-14 21:53:34 +0200

    Created container component for display

---
+++ src/components/ProgressContainer.js
@@ -0,0 +1,25 @@
+import { getDoc, doc } from "firebase/firestore";
+import { db, auth } from "../firebase";
+import { useState, useEffect } from "react";
+import Progress from "./Progress";
+
+const ProgressContainer = () => {
+    const [ workoutHistory, setWorkoutHistory ] = useState([]);
+
+    useEffect(() => {
+        getAndSetWorkoutHistory();
+    }, []);
+
+    const getAndSetWorkoutHistory = async () => {
+        const data = await getDoc(doc(db, auth.currentUser.uid, "routines"))
+        .catch(err => console.log(err));
+
+        setWorkoutHistory(data.data().workoutHistory);
+    }
+
+    return (
+        <Progress workoutHistory={workoutHistory}/>
+    )
+}
+
+export default ProgressContainer;
