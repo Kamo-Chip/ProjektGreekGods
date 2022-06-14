179cb79f39991c68933ae33177375de4ff100c54
Kamogelo Khumalo <kamokhumalo04@gmail.com>
2022-06-14 21:53:13 +0200

    Changed to display component

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,7 +1,54 @@
-const Progress = () => {
+import { Link } from "react-router-dom";
+
+const Progress = ({ workoutHistory }) => {
+    const groupedWorkoutHistory = {};
+
+    const getTitles = () => {
+        let titles = [];
+        workoutHistory.forEach(element => {
+            if(!titles.includes(element.title)){
+                titles.push(element.title);
+            }
+        });
+        return titles;
+    }
+
+    const titles = getTitles();
+
+    const getWorkoutByTitle = (title) => {
+        let workouts = [];
+
+        workoutHistory.forEach(element => {
+            if(element.title === title){
+                workouts.push(element)
+            }
+        });
+
+        return workouts;
+    }
+
+    const groupWorkouts = () => {
+
+        titles.forEach(element => {
+            groupedWorkoutHistory[element] = getWorkoutByTitle(element)
+        })
+    }
+
+    groupWorkouts();
+
     return (
         <div>
-            Progress
+            <h1 className="page-header">Workout History</h1>
+            {Object.keys(groupedWorkoutHistory).map(element => {
+                // console.log(Object.values(groupedWorkoutHistory[element]))
+                return (
+                    <Link to={{pathname:`/progress/${element}`, state: {keys: Object.values(groupedWorkoutHistory[element])}}}>
+                        <div className="workout">
+                            <p>{element}</p>
+                        </div>
+                    </Link>
+                )
+            })}
         </div>
     )
 }
