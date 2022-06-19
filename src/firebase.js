c93240d131a7dc3215ab8dfedd8ae88fa77a501b
Kamogelo Khumalo <kamokhumalo04@gmail.com>
2022-06-19 22:18:15 +0200

    used environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyASYSPc4ylqsIxpFIR-kXfiZ43ksWdX_Wc",
-  authDomain: "workout-log-74ba9.firebaseapp.com",
-  projectId: "workout-log-74ba9",
-  storageBucket: "workout-log-74ba9.appspot.com",
-  messagingSenderId: "1081727390126",
-  appId: "1:1081727390126:web:71a45927d6b95eea93b83f"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
